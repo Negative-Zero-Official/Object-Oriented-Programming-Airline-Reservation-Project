68093b0736a6e2ffff75c688383211fff9b716eb
ItzMeSamYT <pgaitonde05@gmail.com>
2024-11-04 23:07:35 +0530

    v 0.5.4 "Added check for flight status not being 'Cancelled' in Traveler's filter method."

--- com/users/Traveler.java
+++ com/users/Traveler.java
@@ -31,7 +31,7 @@ public class Traveler extends User {
     public void filter(DATE DoT, String origin, String destination) throws InvalidChoiceException {
         filtered = new FlightSchedule();
         for (Flight f : mainFlightSchedule.flightList) {
-            if (f != null && f.origin.equalsIgnoreCase(origin) && f.destination.equalsIgnoreCase(destination) && f.date.equals(DoT)) {
+            if (f != null && f.origin.equalsIgnoreCase(origin) && f.destination.equalsIgnoreCase(destination) && f.date.equals(DoT) && !f.status.equalsIgnoreCase("Cancelled")) {
                 filtered.addFlight(f);
             }
         }
